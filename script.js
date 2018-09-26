b4ea1f647f77e65f49e3ef87fcc7836327da71d5
Colin Armstrong <colinwarmstrong@gmail.com>
2018-09-26 15:16:25 -0600

    Phase 3 is complete; game is fully functional with dynamic ranges. Moving to refactoring.

--- script.js
+++ script.js
@@ -1,78 +1,128 @@
+var lowerRange = 1;
+var upperRange = 100;
+
+var setRangeButton  = document.getElementById('range-button');
+var lowerRangeField = document.getElementById('lower-range-field')
+var upperRangeField = document.getElementById('upper-range-field')
+var guessField      = document.getElementById('guess-field');
+var guessButton     = document.getElementById('guess-button');
+var clearButton     = document.getElementById('clear-button');
+var resetButton     = document.getElementById('reset-button');
+var infoSection     = document.getElementById('info-section');
+var displayNumber   = document.getElementById('last-guess-number');
+var displayMessage  = document.getElementById('guess-response');
+
 var randomNumber = generateRandomNumber();
 
-var resetButton = document.getElementById('reset-button');
-var infoSection = document.getElementById('info-section');
-var clearButton = document.getElementById('clear-button');
-var guessButton = document.getElementById('guess-button');
-var numberField = document.getElementById('number-field');
+lowerRangeField.placeholder = `Set Minimum (${lowerRange})`;
+upperRangeField.placeholder = `Set Maximum (${upperRange})`;
 
-numberField.focus();
+guessField.focus();
 
 function generateRandomNumber() {
-  var randomDecimal = Math.random() * 100;
-  return Math.ceil(randomDecimal);
+  var number =  Math.random() * (upperRange - lowerRange) + lowerRange;
+  return Math.round(number)
 }
 
 function submitGuess(guess) {
-  var infoSection = document.getElementById('info-section')
-  var displayNumber = document.getElementById('last-guess-number')
-  var displayMessage = document.getElementById('guess-response')
-  var resetButton = document.getElementById('reset-button')
   displayNumber.innerText = guess
   if (isNaN(guess)) {
     displayNumber.innerText = 'ERROR';
     displayMessage.innerText = 'Please enter a valid number';
-  } else if (guess < 1 || guess > 100) {
+  } else if (guess < lowerRange || guess > upperRange) {
     displayNumber.innerText = 'ERROR';
-    displayMessage.innerText = 'Guess must be between 1 and 100';
+    displayMessage.innerText = `Guess must be between ${lowerRange} and ${upperRange}`;
   } else if (guess > randomNumber) {
     displayMessage.innerText = 'That is too high';
-    console.log(randomNumber);
   } else if (guess < randomNumber) {
     displayMessage.innerText = 'That is too low';
-    console.log(randomNumber);
   } else {
-    displayMessage.innerText = 'BOOM!';
-    console.log(randomNumber);
+    roundWin();
   }
   infoSection.style.display = 'block';
-  numberField.value = '';
+  guessField.value = '';
   resetButton.disabled = false;
 }
 
-document.getElementById('guess-button').addEventListener('click', function() {
-  var guess = document.getElementById('number-field').value;
-  var userGuess = parseInt(guess);
-  submitGuess(userGuess);
-  numberField.focus();
-  clearButton.disabled = true;
-  guessButton.disabled = true;
-})
+function roundWin() {
+  lowerRange -= 10;
+  upperRange += 10;
+  setMinMax();
+  resetGame();
+  displayMessage.innerText = `BOOM!\nA new random number has been generated between ${lowerRange} and ${upperRange}`;
+}
 
-document.getElementById('reset-button').addEventListener('click', function() {
-  document.getElementById('number-field').value = '';
+function resetGame() {
+  guessField.value = '';
   infoSection.style.display = 'none';
   resetButton.disabled = true;
   clearButton.disabled = true;
   guessButton.disabled = true;
   randomNumber = generateRandomNumber();
-  numberField.focus();
-})
+  guessField.focus();
+}
 
-document.getElementById('clear-button').addEventListener('click', function() {
-  document.getElementById('number-field').value = '';
-  var clearButton = document.getElementById('clear-button');
+function setMinMax() {
+  guessField.min = lowerRange;
+  guessField.max = upperRange;
+  lowerRangeField.placeholder = `Set Minimum (${lowerRange})`;
+  upperRangeField.placeholder = `Set Maximum (${upperRange})`;
+  guessField.placeholder = `Enter a guess between ${lowerRange} and ${upperRange}`;
+}
+
+guessButton.addEventListener('click', function() {
+  var guess = guessField.value;
+  var userGuess = parseInt(guess);
+  submitGuess(userGuess);
+  guessField.focus();
   clearButton.disabled = true;
   guessButton.disabled = true;
-  numberField.focus();
 })
 
-numberField.addEventListener('input', function () {
-  if (numberField.value === "") {
+resetButton.addEventListener('click', function() {
+  resetGame();
+})
+
+clearButton.addEventListener('click', function() {
+  guessField.value = '';
+  clearButton.disabled = true;
+  guessButton.disabled = true;
+  guessField.focus();
+})
+
+guessField.addEventListener('input', function () {
+  if (guessField.value === "") {
     clearButton.disabled = true;
     guessButton.disabled = true;
   } else {
     clearButton.disabled = false;
     guessButton.disabled = false;
+  }
+})
+
+setRangeButton.addEventListener('click', function() {
+  lowerRange = parseInt(lowerRangeField.value);
+  upperRange = parseInt(upperRangeField.value);
+  randomNumber = generateRandomNumber();
+  setRangeButton.disabled = true;
+  lowerRangeField.value = '';
+  upperRangeField.value = '';
+  setMinMax();
+  resetGame();
+})
+
+lowerRangeField.addEventListener('input', function() {
+  if (lowerRangeField.value === '' || upperRangeField.value === '') {
+    setRangeButton.disabled = true;
+  } else {
+    setRangeButton.disabled = false;
+  }
+})
+
+upperRangeField.addEventListener('input', function() {
+  if (lowerRangeField.value === '' || upperRangeField.value === '') {
+    setRangeButton.disabled = true;
+  } else {
+    setRangeButton.disabled = false;
   }
 })
